179b2c7079de20593c5b2b8c6f9e181214708769
Tadeo <tade25.molinas@gmail.com>
2023-02-27 20:52:13 -0300

    ya no importa

--- flask_app/static/js/script.js
+++ flask_app/static/js/script.js
@@ -1,4 +1,11 @@
 window.onload = function () {
+    Swal.fire({
+        title: 'Error!',
+        text: 'Do you want to continue',
+        icon: 'error',
+        confirmButtonText: 'Cool'
+      })
+
     hola = setTimeout(mensajes, 5000)
 };
 
