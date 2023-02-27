3a09ff6351e85ec4bcb15ccc3f4bff04cfc3ab0a
Molinas98 <117995163+Molinas98@users.noreply.github.com>
2023-02-27 19:08:57 -0300

    a ver

--- flask_app/controllers/mensajes.py
+++ flask_app/controllers/mensajes.py
@@ -8,10 +8,7 @@ import time
 direccion = path.abspath(path.dirname(__file__))
 carpetas = direccion.split("\\")
 direccion = direccion[:(len(direccion)-len(carpetas[len(carpetas)-1])-1)]
-notificacion_base = Notify(
-    default_notification_icon=path.join(direccion, 'static\\img', "icono_notificacion.png"),
-    default_notification_audio=path.join(direccion, 'static\\audio', "notificacion.wav"),
-)
+notificacion_base = Notify()
 
 def enviar_notificacion(titulo, mensaje):
     notificacion_base.title = titulo
