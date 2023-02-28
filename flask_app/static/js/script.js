1269d415c9cf4939920c34d982f0433a8499004d
Tadeo <tade25.molinas@gmail.com>
2023-02-28 20:50:35 -0300

    notificacion2

--- flask_app/static/js/script.js
+++ flask_app/static/js/script.js
@@ -6,11 +6,49 @@ window.onload = function () {
     //     confirmButtonText: 'Cool'
     //   })
 
-    hola = setTimeout(mensajes, 5000)
+    //hola = setTimeout(mensajes, 5000)
+    const button = document.querySelector("#boton")
+    button.addEventListener("click", function(){
+        notificacion()
+    });
 };
 
-function mensajes(){
-    fetch("http://54.237.4.187/process")
-    .then( response => response.json() )
-    .then( data => console.log(data) )
-}s
+// function mensajes(){
+//     fetch("http://54.237.4.187/process")
+//     .then( response => response.json() )
+//     .then( data => console.log(data) )
+// }
+
+function notificacion(){
+
+    var propiedades = {
+        body: "Tienes un nuevo mensaje",
+         icon: "i.png"
+        //icon: "http://xitrus.es/imgs/logo_claro.png"
+    }
+
+    if(!("Notification" in window)){
+        alert("El navegador no soporta notificaciones");
+    }else if (Notification.permission === "granted"){
+        var notificacion = new Notification("Mi primera notificación", propiedades) 
+
+        notificacion.onclick = (ev) => {
+            ev.preventDefault()
+            window.open("https://www.instagram.com")
+        }
+        
+    }else if( Notification.permission !== "denied"){
+        Notification.requestPermission(function(permission){
+            if (Notification.permission === "granted"){
+                var notificacion = new Notification("Hola mundo", propiedades) 
+
+                notificacion.onclick = (ev) => {
+                    ev.preventDefault()
+                    window.open("https://www.instagram.com")
+                }
+            }
+        });
+    }
+
+
+}
