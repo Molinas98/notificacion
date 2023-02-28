cf94d2846a2939a056263c1a68a96a6e590f964b
Tadeo <tade25.molinas@gmail.com>
2023-02-27 22:36:10 -0300

    ya no mas

--- flask_app/controllers/mensajes.py
+++ flask_app/controllers/mensajes.py
@@ -9,13 +9,11 @@ direccion = path.abspath(path.dirname(__file__))
 carpetas = direccion.split("\\")
 direccion = direccion[:(len(direccion)-len(carpetas[len(carpetas)-1])-1)]
 
-notificacion_base = Notify(
+def enviar_notificacion(titulo, mensaje):
+    notificacion_base = Notify(
     # default_notification_icon=path.join(direccion, 'static\\img', "icono_notificacion.png"),
     # default_notification_audio=path.join(direccion, 'static\\audio', "notificacion.wav"),
-)
-
-
-def enviar_notificacion(titulo, mensaje):
+    )
     notificacion_base.title = titulo
     notificacion_base.message = mensaje
     notificacion_base.send()
@@ -34,6 +32,10 @@ def login():
 
 @app.route("/process")
 def logina():
+    noti = Notify()
+    noti.title = "Porfa"
+    noti.message = "Aunque sea vos"
+    noti.send()
     for mensaje in mensajes:
         enviar_notificacion(mensaje["titulo"],mensaje["mensaje"])
     return redirect("/")
